73ab12fb33a39b707671874e2dcf4ffbb490dc61
Wrench56 <dmarkreg@gmail.com>
2023-01-29 20:07:08 +0100

    Updated akinsho/bufferline.nvim to v3

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -15,8 +15,8 @@ return require('packer').startup(function(use)
   }
   use {
     'akinsho/bufferline.nvim',
-    tag = 'v2.*',
-    requires = 'kyazdani42/nvim-web-devicons'
+    tag = "v3.*",
+    requires = 'nvim-tree/nvim-web-devicons'
   }
   use {
     'kyazdani42/nvim-tree.lua',
