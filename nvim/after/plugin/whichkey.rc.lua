a1eb688adc6715bf82a16de542ea9d1babb2609d
Wrench56 <dmarkreg@gmail.com>
2023-01-31 00:25:22 +0100

    Removed whichkey's bg & added border & added dashboard keymap

--- nvim/after/plugin/whichkey.rc.lua
+++ nvim/after/plugin/whichkey.rc.lua
@@ -1,11 +1,22 @@
 local status, wk = pcall(require, 'which-key')
 if (not status) then return end
 
+vim.api.nvim_set_hl(0, "WhichKeyFloat", { ctermfg=0,  ctermbg=0 })
+
+wk.setup({
+    window = {
+        border = 'rounded'
+    }
+})
+
 local mappings = {
     e = {':NvimTreeToggle<CR>', 'Toggle filetree'},
-    t = {':Telescope find_files<CR>', 'Open Telescope'}
+    t = {':Telescope find_files<CR>', 'Open Telescope'},
+    d = {':Dashboard<CR>', 'Open Dashboard'}
 }
 
-local opts = {prefix = '<leader>'}
+local opts = {
+    prefix = '<leader>'
+}
 
 wk.register(mappings, opts)
