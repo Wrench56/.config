55e5f271a14af76cbf71def5d4e7a8e8a2faf5d4
Wrench56 <dmarkreg@gmail.com>
2023-01-30 19:18:38 +0100

    Migrated from packer to lazy (package manager)

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,39 +1,45 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'shaunsingh/nord.nvim'
-  use {
+vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  'shaunsingh/nord.nvim',
+  {
     'nvim-treesitter/nvim-treesitter',
-    run = function()
+    build = function()
         local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
         ts_update()
     end,
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'kyazdani42/nvim-web-devicons', opt = true}
-  }
-  use {
+  },
+  {
     'akinsho/bufferline.nvim',
     tag = "v3.*",
-    requires = 'nvim-tree/nvim-web-devicons'
-  }
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    dependencies = 'nvim-tree/nvim-web-devicons'
+  },
+  {
+    'nvim-tree/nvim-web-devicons',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
     },
     tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-  use 'glepnir/dashboard-nvim'
-  use {
+  },
+  {
+    'glepnir/dashboard-nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+  },
+
+
+  {
     'nvim-telescope/telescope.nvim',
-    requires = { 'nvim-lua/plenary.nvim' }
-  }
-  use 'norcalli/nvim-colorizer.lua'
-  use 'lewis6991/impatient.nvim' -- hopefully, speeds up everything (not that if nvim would be slow lol)
-  use 'windwp/nvim-ts-autotag'
-  use 'p00f/nvim-ts-rainbow'
-  use 'windwp/nvim-autopairs'
-  use 'folke/which-key.nvim'
-end)
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
+  {
+    'nvim-lualine/lualine.nvim',
+    lazy = true
+  },
+  'norcalli/nvim-colorizer.lua',
+  'windwp/nvim-ts-autotag',
+  'p00f/nvim-ts-rainbow',
+  'windwp/nvim-autopairs',
+  'folke/which-key.nvim',
+
+})
