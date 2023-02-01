a5f18e97837928faae5c05ba6047834bc1c840c9
Wrench56 <dmarkreg@gmail.com>
2023-02-01 18:48:28 +0100

    Fixed WSL (1) error message on update

--- .setup/fix_wsl.sh
+++ .setup/fix_wsl.sh
@@ -1,3 +1,6 @@
 # Reinstall gzip
 sudo apt-get purge gzip --allow-remove-essential
 sudo apt-get install gzip
+
+# Fixes the "Failed to retrieve available kernel versions." error
+sudo apt-get purge needrestart
