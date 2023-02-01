e63a5ce19e831492271cb448764d2df6886bdbb4
Wrench56 <dmarkreg@gmail.com>
2023-02-01 21:06:05 +0100

    Added lazygit installation

--- .setup/setup.sh
+++ .setup/setup.sh
@@ -58,6 +58,8 @@ sudo apt-get install neovim
     cargo install tree-sitter-cli
 
     # Install lazygit
-    sudo add-apt-repository ppa:lazygit-team/daily
-    sudo apt-get update
-    sudo apt-get install lazygit
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep '"tag_name":' |  sed -E 's/.*"v*([^"]+)".*/\1/')
+    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    sudo tar xf lazygit.tar.gz -C /usr/local/bin lazygit
+    lazygit --version
+    rm lazygit.tar.gz
