7aeb88849a1cffda98c019b20a79510c7808ee81
Wrench56 <dmarkreg@gmail.com>
2023-02-06 02:34:11 +0100

    Copied whichkey keymaps for LSP

--- nvim/after/plugin/whichkey.rc.lua
+++ nvim/after/plugin/whichkey.rc.lua
@@ -19,7 +19,28 @@ local mappings = {
         t = {':ToggleTerm direction=tab<CR>', 'Open Bash in Tab'},
         s = {':ToggleTerm size=40 direction=vertical<CR>', 'Open Bash on Side'}
     },
-    d = {':Dashboard<CR>', 'Open Dashboard'}
+    d = {':Dashboard<CR>', 'Open Dashboard'},
+    l = {
+        name = 'LSP Settings',
+        i = {':LspInfo<CR>', 'List connected language servers'},
+        k = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "Signature Help" },
+        K = { "<cmd>Lspsaga hover_doc<cr>", "Hover Commands" },
+        w = { '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>', "Add Workspace Folder" },
+        W = { '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>', "Remove Workspace Folder" },
+        l = {
+          '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>',
+          "List Workspace Folders"
+        },
+        t = { '<cmd>lua vim.lsp.buf.type_definition()<cr>', "Type Definition" },
+        d = { '<cmd>lua vim.lsp.buf.definition()<cr>', "Go To Definition" },
+        D = { '<cmd>lua vim.lsp.buf.declaration()<cr>', "Go To Declaration" },
+        r = { '<cmd>lua vim.lsp.buf.references()<cr>', "References" },
+        R = { '<cmd>Lspsaga rename<cr>', "Rename" },
+        a = { '<cmd>Lspsaga code_action<cr>', "Code Action" },
+        e = { '<cmd>Lspsaga show_line_diagnostics<cr>', "Show Line Diagnostics" },
+        n = { '<cmd>Lspsaga diagnostic_jump_next<cr>', "Go To Next Diagnostic" },
+        N = { '<cmd>Lspsaga diagnostic_jump_prev<cr>', "Go To Previous Diagnostic" }
+    }
 }
 
 local opts = {
