ba7a2cda9d6c4c56b49dcfc27cc27badd15ae1c4
Wrench56 <dmarkreg@gmail.com>
2023-02-06 01:56:24 +0100

    Updated autopairs.rc.lua because of nvim-cmp

--- nvim/after/plugin/autopairs.rc.lua
+++ nvim/after/plugin/autopairs.rc.lua
@@ -4,3 +4,10 @@ if (not status) then return end
 autopairs.setup({
     map_cr = true
 })
+
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local cmp = require('cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
