8451269e48f80df57155fe9845d77a9213a829c8
Wrench56 <dmarkreg@gmail.com>
2023-02-06 02:03:12 +0100

    Added custom cmp plugin diagnostic signs

--- nvim/after/plugin/cmp.rc.lua
+++ nvim/after/plugin/cmp.rc.lua
@@ -93,6 +93,14 @@ cmp.setup.cmdline(':', {
     })
 })
 
+local signs = {Error = " ", Warn = " ", Hint = " ", Info = " "}
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, {text = icon, texthl = hl, numhl = ""})
+end
+
+
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
