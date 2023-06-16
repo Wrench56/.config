b86009b79d2cfa08d3aa72b18c7303663efeab56
Wrench56 <dmarkreg@gmail.com>
2023-06-16 03:39:46 +0200

    Removed indentation on dashboard screen

--- nvim/after/plugin/indent.rc.lua
+++ nvim/after/plugin/indent.rc.lua
@@ -1,6 +1,8 @@
 local status, indent = pcall(require, 'nvim-indent-blankline')
 if (not status) then return end
 
+vim.g.indent_blankline_filetype_exclude = {'dashboard'}
+
 indent.setup({
     show_current_context = true,
     show_current_context_start = true,
